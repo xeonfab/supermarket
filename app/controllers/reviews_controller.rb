4572261f1352534842983c60cb4ffeb8d2595e59
Fabien Riou <fabien.riou92@gmail.com>
2020-02-27 12:22:43 +1100

    changed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+  def new
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new
+  end
+  def create
+    @booking = Booking.find(params[:booking_id])
+    @review = Review.new(review_params)
+    @review.booking = @booking
+    @review.user = current_user
+    if @review.save!
+    redirect_to account_my_bookings_path(@booking)
+    else
+      render :new
+    end
+  end
+  private
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
 end
+
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :booking
+  validates :description, presence: true
+  validates :rating,
+            presence: true,
+            inclusion: { in: [0, 1, 2, 3, 4, 5] },
+            numericality: { only_integer: true }
 end
